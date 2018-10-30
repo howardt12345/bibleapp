9da23b96ee98096bd0908dd3f0a805ccd97c2fa6
howardt12345 <howardt12345@users.noreply.github.com>
2018-10-29 20:51:42 -0400

    Attempt at fixing multidex issue

---
+++ android/app/src/main/java/com/howardt12345/bible/MyApplication.java
@@ -0,0 +1,32 @@
+package com.howardt12345.bible;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.annotation.CallSuper;
+
+import io.flutter.view.FlutterMain;
+
+public class MyApplication extends Application {
+    @Override
+    @CallSuper
+    public void onCreate() {
+        super.onCreate();
+        FlutterMain.startInitialization(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    private Activity mCurrentActivity = null;
+    public Activity getCurrentActivity() {
+        return mCurrentActivity;
+    }
+    public void setCurrentActivity(Activity mCurrentActivity) {
+        this.mCurrentActivity = mCurrentActivity;
+    }
+}
